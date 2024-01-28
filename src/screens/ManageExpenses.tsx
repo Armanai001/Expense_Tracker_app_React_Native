166120ef1908ba84fa3d7aa4f24048552c08cdbc
Arman <theaiemperor@gmail.com>
2024-01-28 16:09:41 +0530

    Setting up Project

---
+++ src/screens/ManageExpenses.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from "react-native";
+
+export default function ManageExpenses() {
+    return <>
+        <View>
+            <Text>
+                Manage Expenses
+            </Text>
+        </View>
+    </>
+}
